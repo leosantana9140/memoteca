3ba7269c1bee9a2a2c768f7df54282baee4931e8
leosantana9140 <leosantana9140@gmail.com>
2023-04-21 17:03:30 -0300

    Criação de cabeçalho e rodapé.

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Leonardo Santana',
+    modelo: 'modelo1'
+  }
+
+  criarPensamento() {
+    alert('Novo pensamento criado!')
+  }
+
+  cancelar() {
+    alert('Ação cancelada!')
+  }
+
+}
